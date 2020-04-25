268087711e0188649cb8241369d1f1eefcf0c18c
Pink Moonlight <kate.cherie25@gmail.com>
2020-04-24 17:37:15 -0700

    Updates

---
+++ precache-manifest.4136fdd0c13ebbc8312680ab5abfbbe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93164943f23832c59b1a",
+    "url": "/Super-Sticky-Notes/static/css/main.243fcca8.chunk.css"
+  },
+  {
+    "revision": "93164943f23832c59b1a",
+    "url": "/Super-Sticky-Notes/static/js/main.38af150c.chunk.js"
+  },
+  {
+    "revision": "5d8b7d092302b0042906",
+    "url": "/Super-Sticky-Notes/static/js/runtime~main.7374a95a.js"
+  },
+  {
+    "revision": "25a6fa89cb8626587fce",
+    "url": "/Super-Sticky-Notes/static/js/2.0d7b7b53.chunk.js"
+  },
+  {
+    "revision": "f8635a15d4aedb1bef845bcc157d6a2d",
+    "url": "/Super-Sticky-Notes/index.html"
+  }
+];
